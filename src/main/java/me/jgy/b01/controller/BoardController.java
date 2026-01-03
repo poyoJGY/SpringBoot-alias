cc8c67c56461cc39c8efa5ab77238d4fbda938bd
poyoJGY <duke407@naver.com>
2026-01-03 18:03:28 +0900

    Modify and Remove function, etc.

--- src/main/java/me/jgy/b01/controller/BoardController.java
+++ src/main/java/me/jgy/b01/controller/BoardController.java
@@ -1,5 +1,6 @@
 package me.jgy.b01.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import me.jgy.b01.dto.BoardDTO;
@@ -8,8 +9,11 @@ import me.jgy.b01.dto.PageResponseDTO;
 import me.jgy.b01.service.BoardService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/board")
@@ -27,5 +31,82 @@ public class BoardController {
         log.info(responseDTO);
 
         model.addAttribute("responseDTO", responseDTO);
+    }
+
+    @GetMapping("/register")
+    public void registerGET() {
+
+    }
+
+    @PostMapping("/register")
+    public String registerPost(@Valid BoardDTO boardDTO, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+
+        log.info("board POST register...............");
+
+        if (bindingResult.hasErrors()) {
+            log.info("has errors..........");
+            redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
+
+            return "redirect:/board/register";
+        }
+
+        log.info(boardDTO);
+
+        Long bno = boardService.register(boardDTO);
+
+        redirectAttributes.addFlashAttribute("result", bno);
+
+        return "redirect:/board/list";
+    }
+
+    @GetMapping({"/read", "/modify"})
+    public void read(Long bno, PageRequestDTO pageRequestDTO, Model model) {
+
+        BoardDTO boardDTO = boardService.readOne(bno);
+
+        log.info(boardDTO);
+
+        model.addAttribute("dto", boardDTO);
+    }
+
+    @PostMapping("/modify")
+    public String modify(PageRequestDTO pageRequestDTO,
+                         @Valid BoardDTO boardDTO,
+                         BindingResult bindingResult,
+                         RedirectAttributes redirectAttributes) {
+
+        log.info("board modify post......." + boardDTO);
+
+        if (bindingResult.hasErrors()) {
+            log.info("has errors.......");
+
+            String link = pageRequestDTO.getLink();
+
+            redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
+
+            redirectAttributes.addAttribute("bno", boardDTO.getBno());
+
+            return "redirect:/board/modify?" + link;
+        }
+
+        boardService.modify(boardDTO);
+
+        redirectAttributes.addFlashAttribute("result", "modified");
+
+        redirectAttributes.addAttribute("bno", boardDTO.getBno());
+
+        return "redirect:/board/read";
+    }
+
+    @PostMapping("/remove")
+    public String remove(Long bno, RedirectAttributes redirectAttributes) {
+
+        log.info("remove post.. " + bno);
+
+        boardService.remove(bno);
+
+        redirectAttributes.addFlashAttribute("result", "removed");
+
+        return "redirect:/board/list";
     }
 }
--- src/main/java/me/jgy/b01/dto/BoardDTO.java
+++ src/main/java/me/jgy/b01/dto/BoardDTO.java
@@ -1,5 +1,7 @@
 package me.jgy.b01.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,9 +16,16 @@ import java.time.LocalDateTime;
 public class BoardDTO {
 
     private Long bno;
+    @NotEmpty
+    @Size(min = 3, max = 100)
     private String title;
+
+    @NotEmpty
     private String content;
+
+    @NotEmpty
     private String writer;
+
     private LocalDateTime regDate;
     private LocalDateTime modDate;
 }
